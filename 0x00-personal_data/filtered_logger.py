da255045c6676cd0c055655b2e9b7e4eb41ff0e0
root <root@DESKTOP-B19RQ5P>
2024-09-14 18:35:51 +0300

    Secure database connection

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,8 @@
 from typing import List
 import re
 import logging
+import os
+import mysql.connector
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -50,3 +52,19 @@ def get_logger() -> logging.Logger:
     streamhandler.setFormatter(formatter)
     logger.addHandler(streamhandler)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Return a connector to the database."""
+    print("User:", os.getenv('PERSONAL_DATA_DB_USERNAME'))
+    print("Password:", os.getenv('PERSONAL_DATA_DB_PASSWORD'))
+    print("Host:", os.getenv('PERSONAL_DATA_DB_HOST'))
+    print("Database:", os.getenv('PERSONAL_DATA_DB_NAME'))
+    db_config = {
+            'user': os.getenv('PERSONAL_DATA_DB_USERNAME'),
+            'password': os.getenv('PERSONAL_DATA_DB_PASSWORD'),
+            'host': os.getenv('PERSONAL_DATA_DB_HOST'),
+            'database': os.getenv('PERSONAL_DATA_DB_NAME')
+    }
+    database_connection = mysql.connector.connect(**db_config)
+    return database_connection

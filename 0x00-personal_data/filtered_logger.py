bb5b5d0c9c23bc22c18773f867806badf2d3354e
root <root@DESKTOP-B19RQ5P>
2024-09-14 20:55:10 +0300

    trying to fix acess denied

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -53,27 +53,6 @@ def get_logger() -> logging.Logger:
     logger.addHandler(streamhandler)
     return logger
 
-"""
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    Return a connector to the database.
-    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
-    try:
-        database_connection = mysql.connector.connect(
-                host=db_host,
-                port=3306,
-                user=db_user,
-                password=db_password,
-                database=db_name,
-        )
-        return database_connection
-    except mysql.connector.Error as err:
-        print(f"Error connecting to MySQL: {err}")
-        print(f"Connection details: host={db_host}, user={db_user}, database={db_name}")
-        raise
-"""
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """Creates a connector to a database.
     """

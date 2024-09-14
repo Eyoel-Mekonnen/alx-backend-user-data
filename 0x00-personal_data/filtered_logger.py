d50a75bec60adebe1790066c5ed44bc05b659bbd
root <root@DESKTOP-B19RQ5P>
2024-09-14 14:52:22 +0300

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """Personal Data handled."""
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction:
@@ -13,3 +14,24 @@ def filter_datum(fields: List[str], redaction:
     redact = r"\1=" + redaction
     match = re.sub(pattern, redact, message)
     return match
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class."""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[list]):
+        """Intialize the constructor."""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filter the log received to remove sensitive data."""
+        result = filter_datum(self.fields, RedactingFormatter.REDACTION,
+                              record.msg, RedactingFormatter.SEPARATOR)
+        record.msg = result
+        formatted_message = super(RedactingFormatter, self).format(record)
+        return formatted_message

b3ef92ad5722a01913315dae592939477c9eb8c5
root <root@DESKTOP-B19RQ5P>
2024-09-16 00:48:44 +0300

    checking paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,14 @@ class Auth:
     """Class that contain the template for auth."""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Handle requirement of auth."""
-        return False
+        if path is None or excluded_paths is None:
+            return True
+        if path[len(path) - 1] != '/':
+            path = path + '/'
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """Handle authorization of header."""

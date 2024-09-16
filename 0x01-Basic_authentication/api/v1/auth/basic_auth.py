d088b9b48124cc141f7ecc52cb879b783bbea318
root <root@DESKTOP-B19RQ5P>
2024-09-16 14:57:06 +0300

    decode base 64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Class for basic Authentication."""
 from api.v1.auth.auth import Auth
+import binascii
+import base64
 
 
 class BasicAuth(Auth):
@@ -17,3 +19,18 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization.split()[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """Decode base64 encoded authorization."""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            decoded = decoded.decode('utf-8')
+            return decoded
+        except (binascii.Error, UnicodeDecodeError):
+            return None

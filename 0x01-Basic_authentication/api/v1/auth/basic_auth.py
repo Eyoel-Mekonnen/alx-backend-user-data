24459446bb0d4c16d97fd382b50865d1890beff8
root <root@DESKTOP-B19RQ5P>
2024-09-16 14:39:18 +0300

    base64 encdded

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,14 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Class taht will implement basic auth."""
 
-    pass
+    def extract_base64_authorization_header(self, authorization: str) -> str:
+        """Extraacts the base64 encoded."""
+        if authorization is None:
+            return None
+        if type(authorization) != str:
+            return None
+        header_string = authorization.split()[0]
+        if header_string != 'Basic':
+            return None
+        else:
+            return authorization.split()[1]

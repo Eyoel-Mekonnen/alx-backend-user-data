1586e1ba4db21d3603e7c9cf5d17bfeed70af17b
root <root@DESKTOP-B19RQ5P>
2024-09-22 21:41:10 +0300

    retreive user_id using session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,9 +21,9 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """Return User ID based on session_id."""
-        if sesion_id is None:
+        if session_id is None:
             return None
         if type(session_id) != str:
             return None
-        user_id = SessionAuth[session_id]
+        user_id = SessionAuth.user_id_by_session_id.get(session_id)
         return user_id

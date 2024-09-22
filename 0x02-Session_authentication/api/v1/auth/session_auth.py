d31a0edbaf792d75d010fc69fa5f9cdc354d9fa4
root <root@DESKTOP-B19RQ5P>
2024-09-22 21:28:08 +0300

    documented the class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,11 +3,14 @@
 from api.v1.auth.auth import Auth
 from uuid import uuid4
 
+
 class SessionAuth(Auth):
     """Class session Auth."""
+
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """Create a unique session id to append session id by user."""
         if user_id is None:
             return None
         if type(user_id) != str:
